fd2b5a17f6f489a5ae15c551a4fe01e6167a9d89
kulandaiyesum <kulandaiyesu333@gmail.com>
2024-08-06 15:49:02 +0530

    feat: login page UI

---
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { AuthenticationRequest } from '../../services/models';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  authRequest: AuthenticationRequest = { email: '', password: '' };
+  errorMsg: Array<string> = [];
+  router = inject(Router);
+  authServerce = inject(AuthenticationService);
+
+  login() {}
+  register() {
+    this.router.navigate(['register']);
+  }
+}
