e95dbacfe35e9e0f0e0e3c70e466770aba738384
kulandaiyesum <kulandaiyesu333@gmail.com>
2024-08-08 10:58:16 +0530

    feat: Register and activate-account
    Register and activate account components are added and  package added for showing good looking 6 digit OTP to enter

---
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { CodeInputModule } from 'angular-code-input';
+
+@Component({
+  selector: 'app-activate-account',
+  standalone: true,
+  imports: [CodeInputModule],
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss',
+})
+export class ActivateAccountComponent {
+  message = '';
+  isOkay = true;
+  submitted = false;
+  router: Router = inject(Router);
+  authService = inject(AuthenticationService);
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+  private confirmAccount(token: string) {
+    this.authService
+      .confirm({
+        token,
+      })
+      .subscribe({
+        next: () => {
+          this.message =
+            'Your account has been successfully activated.\nNow you can proceed to login';
+          this.submitted = true;
+          this.isOkay = true
+        },
+        error: () => {
+          this.message = 'Token has been expired or invalid';
+          this.submitted = true;
+          this.isOkay = false;
+        },
+      });
+  }
+}
--- book-network-ui/src/app/pages/register/register.component.ts
+++ book-network-ui/src/app/pages/register/register.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegistrationRequest } from '../../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  registerRequest: RegistrationRequest = {
+    email: '',
+    firstname: '',
+    lastname: '',
+    password: '',
+  };
+  errorMsg: Array<string> = [];
+  router = inject(Router);
+  authService = inject(AuthenticationService);
 
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService
+      .register({
+        body: this.registerRequest,
+      })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          this.errorMsg = err.error.validationErrors;
+        },
+      });
+  }
 }
